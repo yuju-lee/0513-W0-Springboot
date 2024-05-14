80dd02606d04da886f766803b3a3bf588bcf8ea2
yuju-lee <yuju-lee@github.com>
2024-05-14 12:02:18 +0900

    build Create and Read

---
+++ src/main/java/com/sparta/memo/controller/MemoController.java
@@ -0,0 +1,44 @@
+package com.sparta.memo.controller;
+
+import com.sparta.memo.dto.MemoRequestDto;
+import com.sparta.memo.dto.MemoResponseDto;
+import com.sparta.memo.entity.Memo;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+
+@RestController
+@RequestMapping("/api")
+public class MemoController {
+
+    private final Map<Long, Memo> memoList = new HashMap<>();
+
+    @PostMapping("/memos")
+    public MemoResponseDto createMemo(@RequestBody MemoRequestDto requestDto) {
+        // RequestDto -> entity
+        Memo memo = new Memo(requestDto);
+
+        // Memo Max ID Check
+        Long maxId = memoList.size() > 0 ? Collections.max(memoList.keySet()) + 1 : 1;
+        memo.setId(maxId);
+        // save to DB
+        memoList.put(memo.getId(), memo);
+
+        //entity > ResponseDto
+        MemoResponseDto responseDto = new MemoResponseDto(memo);
+
+        return responseDto;
+    }
+
+    @GetMapping("/memos")
+    public List<MemoResponseDto> getMemos() {
+        // Map to List
+        List<MemoResponseDto> responseList = memoList.values().stream().map(MemoResponseDto::new).toList();
+
+        return responseList;
+    }
+
+
+
+
+}
